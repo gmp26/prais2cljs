5993699780fb275b3a945480ccf35e9afc31149b
gmp26 <gmp26@cam.ac.uk>
2015-11-12 17:33:06 +0000

    abortive attempt to add email out

--- src/prais2/logger.cljs
+++ src/prais2/logger.cljs
@@ -1,6 +1,7 @@
 (ns ^:figwheel-always prais2.logger
     (:require [rum.core :as rum]
               [prais2.core :as core]
+
               ))
 
 (rum/defc icon-control [icon event-key tooltip]
@@ -29,6 +30,20 @@
 (rum/defc load-control []
   (icon-control "sign-in fa-rotate-90" :load-session "read in session"))
 
+#_(rum/defcc my-component [comp arg1 arg2]
+  (let [node (.findDOMNode comp)]
+    [:div "fails with: Uncaught TypeError: comp.findDOMNode is not a function"]))
+
+(rum/defcc my-component [comp arg1 arg2]
+  (let [node (.findDOMNode js/React comp)]
+    [:div "fails with: Invariant Violation: findComponentRoot(...): Unable to find element.  This probably means the DOM was unexpectedly mutated ..."]))
+
+#_(rum/defcc div [comp to-address session-log]
+  (let [element (.findDOMNode js/React comp)]
+
+    (.log js/console element)
+    [:div#lookatme "Hi there"]
+))
 
 (rum/defc playback-controls []
   [:div
@@ -42,4 +57,18 @@
     (share-control)
     (load-control)]
 
+   (my-component 1 2)
+
+   #_(email-form "gmp26@cam.ac.uk" "hello there")
    ])
+
+
+(comment
+  #_[:form {:action (str "mailto:" to-address
+                         "?subject=" "prais2-session-start " (new js/Date.))
+            :method "post"
+            :enctype "text/plain"}
+     [:input {:type "text"
+              :name "session log"
+              :value session-log
+              }]])
