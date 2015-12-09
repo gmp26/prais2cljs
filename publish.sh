8672bd6605244da33f61752d5b281dc75d6cae2e
gmp26 <gmp26@cam.ac.uk>
2015-12-09 10:46:37 +0000

    moving to new server

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 cd ~/clojure/prais2
 lein clean
 lein cljsbuild once min
-rsync -av resources/public/ gmp26@maths.org:/www/cms/drupal/sites/understandinguncertainty.org/files/animations/standalone/PRAIS2
+rsync -av resources/public/ gmp26@pan.maths.org:/www/cms/drupal/sites/understandinguncertainty.org/files/animations/standalone/PRAIS2
