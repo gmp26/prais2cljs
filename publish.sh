32eb8292698c4df75f21d6f6b3623edcbfab7079
gmp26 <gmp26@cam.ac.uk>
2016-05-16 17:27:32 +0100

    added static build and sass to the publish script

--- publish.sh
+++ publish.sh
@@ -2,8 +2,10 @@
 
 cd ~/clojure/prais2
 lein clean
+lein home     # creates static home page for fast load
 lein cljsbuild once min
+mkdir -p resources/public/css
+sass sass/app.scss resources/public/css/app.css
 gulp
 #rsync -av resources/public/ gmp26@webuu1.maths.cam.ac.uk:/www/drupal/sites/understandinguncertainty.org/files/animations/standalone/PRAIS2
 rsync -av resources/public/ gmp26@webuu2.maths.cam.ac.uk:/var/www/childrensheartsurgery.info/html/
-# rsync -av PRAIS2s0-1/ gmp26@webuu1.maths.cam.ac.uk:/www/drupal/sites/understandinguncertainty.org/files/animations/standalone/PRAIS2s0-1
