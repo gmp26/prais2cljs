1b2ca56bac40f98502de2cd0ea59a64e1a199c98
mehdi-hajjam <mehdihajjam@gmail.com>
2021-09-16 10:37:10 +0200

    Adding Emacs config file

---
+++ src/prais2/.dir-locals.el
@@ -0,0 +1,4 @@
+((clojurescript-mode . ((cider-default-cljs-repl . figwheel-main)
+			(cider-preferred-build-tool . clojure-cli)
+			(cider-figwheel-main-default-options . ":dev")
+                        (cider-clojure-cli-global-options . "-A:fig"))))
